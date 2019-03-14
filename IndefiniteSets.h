502ff2bbc204f0222c78cd3a02848f9d3874c2fa
SG_Muwa <motherlode.muwa@gmail.com>
2019-03-14 09:55:27 +0300

    Поменял структуры данных

--- IndefiniteSets.h
+++ IndefiniteSets.h
@@ -1,90 +1,90 @@
 ﻿
-// Структура чтобы указать максимальное и минимальное значение.
+#ifndef IndefiniteSets_FLOAT
+#define IndefiniteSets_FLOAT double
+#endif // !IndefiniteSets_FLOAT
+
+#ifndef IndefiniteSets_CHAR
+#define IndefiniteSets_CHAR char
+#endif // !IndefiniteSets_CHAR
+
+// Массив символов. Нечто похожее на строку.
+struct IndefiniteSets_CharArray {
+	// Количество допустимых символов.
+	size_t count;
+	// Указатель на первый символ.
+	IndefiniteSets_CHAR * string;
+};
+
+// Отрезок ОТ и ДО включительно.
 struct IndefiniteSets_Range {
-	// Минимальное значение.
-	double min;
-	// Максимальное значение.
-	double max;
+	// Минимальное допустимое значение.
+	IndefiniteSets_FLOAT min;
+	// Максимальное допустимое значение.
+	IndefiniteSets_FLOAT max;
 };
 
 /*
-Структура представляет из себя массив символов.
+Представление термы нечётких множеств.
+Это некоторое соответсвие:
+Большой это от 180 до 300
 */
-struct IndefiniteSets_CharArray {
-	// Количество символов.
-	size_t size;
-	// Указатель на начало строки.
-	char * array;
-};
-
-// Функция принадлежности (membership function)
-struct IndefiniteSets_MF {
-	// Название функции принадлежности.
+struct IndefiniteSets_Terma {
+	// Имя термы.
 	struct IndefiniteSets_CharArray name;
-	/*
-	Указатель на функцию принадлежности.
-	double x - Основной вход функции.
-	double * output - Указатель на результат функции.
-	void * context - Указатель на контекст к функции.
-	Возвращает: код ошибки функции.
-	*/
-	int (*mf)(double x, double * output, void * context);
-	/*
-	Контекст функции. Будет отправлен последним аргументом функции.
-	*/
-	void * context;
-};
-
-// Тип: массив функций принадлежности (membership function)
-struct IndefiniteSets_MFArray {
-	// Количество функций принадлежности в массиве.
-	size_t size;
-	// Указатель на начало массива функций принадлежности.
-	struct IndefiniteSets_MF * array;
-};
-
-// Массив выходов.
-struct IndefiniteSets_OutputArray {
-	// Количество выходов в массиве.
-	size_t size;
-	// Указатель на первый выход массива.
-	struct IndefiniteSets_Output * array;
-};
-
-// Структура описывает одно можетсво входных данных.
-struct IndefiniteSets_Input {
-	// Минимум и максимум допустимый входной параметр
+	// Разброс, которому соответсвует терма.
 	struct IndefiniteSets_Range range;
-	// Название входного параметра
+};
+
+// Массив терм.
+struct IndefiniteSets_TermaArray {
+	// Границы массива терм.
+	struct IndefiniteSets_Range globalRange;
+	// Количество терм.
+	size_t count;
+	// Указатель на первую терму.
+	struct IndefiniteSets_Terma * terma;
+};
+
+/*
+Правило, которое делает вывод:
+если first, то second.
+*/
+struct IndefiniteSets_Rule {
+	// Основание гипотезы.
+	struct IndefiniteSets_Terma * first;
+	// Гипотеза.
+	struct IndefiniteSets_Terma * second;
+};
+
+// Массив правил нечётких множеств.
+struct IndefiniteSets_RuleArray {
+	// Количество нечётких множеств.
+	size_t count;
+	// Указатель на первое правило между двумя нечёткими множествами.
+	struct IndefiniteSets_Rule * rule;
+};
+
+// Характеристики некоторого одного нечётного множества
+struct IndefiniteSets_Characteristic {
+	// Название нечёткого множества.
 	struct IndefiniteSets_CharArray name;
-	// Массив функций принадлежности (membership function)
-	struct IndefiniteSets_MFArray MFs;
+	// Термы нечёткого множества.
+	struct IndefiniteSets_TermaArray terms;
 };
 
-struct IndefiniteSets_Output {
-	// Минимум и максимум допустимый входной параметр
-	struct IndefiniteSets_Range range;
-	// Название входного параметра
-	struct IndefiniteSets_CharArray name;
-	// Массив функций принадлежности (membership function)
-	struct IndefiniteSets_MFArray MFs;
+// Массив нечётких множеств
+struct IndefiniteSets_CharacteristicArray {
+	// Количество нечётких множеств.
+	size_t count;
+	// Указатель на первую характеристику нечётного множества.
+	struct IndefiniteSets_Characteristic * characteristic;
 };
 
-struct IndefiniteSets_InputArray {
-	// Количество элементов в массиве.
-	size_t size;
-	// Указатель на начало массива.
-	struct IndefiniteSets_Input * array;
-};
-
-// Символизирует состояние системы в целом.
-struct IndefiniteSets_System {
-	// Входные множества в систему.
-	struct IndefiniteSets_InputArray inputs;
-	// Выходные множества из системы.
-	struct IndefiniteSets_OutputArray outputs;
-	// Правила перехода из входных в выходные.
+// Состояние системы нечётких множеств.
+struct IndefiniteSets_State
+{
+	// Массив нечётких множеств.
+	struct IndefiniteSets_CharacteristicArray characteristics;
+	// Правила нечётких множеств.
 	struct IndefiniteSets_RuleArray rules;
-	// Название данной системы.
-	struct IndefiniteSets_CharArray name;
 };
