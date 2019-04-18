19803227d4f217ca0795d8eef98918dad0034fb9
SG_Muwa <motherlode.muwa@gmail.com>
2019-04-18 10:27:34 +0300

    Написал тест вызов функции нечёткого управления

--- UnitTestProject1/UnitTest2.cs
+++ UnitTestProject1/UnitTest2.cs
@@ -1,5 +1,8 @@
 ﻿using System;
+using System.Collections.Generic;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using lab1;
+using lab1.logic.lab2;
 
 namespace UnitTestProject1
 {
@@ -30,7 +33,7 @@ namespace UnitTestProject1
                 0, 4);
 
             Characteristic y =
-                new Characteristic("y", 10, 40);
+                new Characteristic("y", 0, 40);
             state.Characteristics.Add(y);
             y.Add(
                 "Малый", 0, 0,
@@ -50,9 +53,27 @@ namespace UnitTestProject1
             state.Rules.Add(x["Средний"], y["Средний"]);
             state.Rules.Add(x["Большой"], y["Большой"]);
 
-            
-            
+            ICollection<CharacteristicAndValue> outputCollection =
+                state.Rules.Call(x, 1.3);
+            CharacteristicAndValue[] expect = new CharacteristicAndValue[] { new CharacteristicAndValue(y, 163 / 12.1) };
+            Assert.AreEqual(expect.Length, outputCollection.Count);
+            CharacteristicAndValue output = outputCollection.GetFirst();
+            Assert.AreEqual(expect[0].Characteristic, output.Characteristic);
+            Assert.AreEqual(expect[0].Value, output.Value, 0.01);
+
             Console.WriteLine(state);
+        }
+
+        
+    }
+    static class GetFirstInCollection
+    {
+        public static T GetFirst<T>(this IEnumerable<T> list)
+        {
+            IEnumerator<T> en = list.GetEnumerator();
+            if (!en.MoveNext())
+                throw new ArgumentException();
+            return en.Current;
         }
     }
 }
