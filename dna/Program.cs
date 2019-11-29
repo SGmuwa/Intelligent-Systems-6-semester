37ea9a465bea723f576eea81ab5017ec778bc505
SG_Muwa <motherlode.muwa@gmail.com>
2019-11-29 15:36:30 +0300

    org.mariuszgromada.math.mxparser

---
+++ dna/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using org.mariuszgromada.math.mxparser;
+
+namespace dna
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Function My = new Function("My(a) = a*2");
+            Argument a = new Argument(nameof(a), 2);
+            Console.WriteLine(My.calculate(a));
+        }
+    }
+}
